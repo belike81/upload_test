2928da6f56e0f162568510d6c9af399adbeaad0b
Tomasz Grabowski <belike81@gmail.com>
2012-07-11 18:42:15 +0200

    Further changes, added log dir

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,9 @@
-# define paths and filenames
 deploy_to = "/home/deployer/apps/upload_test"
-pid_file = "#{deploy_to}/shared/pids/unicorn.pid"
-err_log = "#{deploy_to}/log/unicorn.log"
-log_file = "#{deploy_to}/log/unicorn.log"
-old_pid = pid_file + '.oldbin'
+working_directory "#{deploy_to}/current"
+pid "#{deploy_to}/shared/pids/unicorn.pid"
+stderr_path "#{deploy_to}/shared/log/unicorn.log"
+stdout_path "#{deploy_to}/shared/log/unicorn.log"
+listen "/tmp/unicorn.upload_test.sock"
 
 timeout 30
 worker_processes 2 # increase or decrease
-
-working_directory "#{deploy_to}/current"
-pid pid_file
-stderr_path err_log
-stdout_path log_file
-listen "/tmp/unicorn.upload_test.sock"
